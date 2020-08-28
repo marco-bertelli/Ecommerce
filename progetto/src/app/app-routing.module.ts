dfbecffddd8592dac9496e08fb232208a0f36bde
unknown <marcobert37@gmail.com>
2020-08-28 15:55:38 +0200

    modulo per dettaglio/modificare

--- progetto/src/app/app-routing.module.ts
+++ progetto/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
   { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'registrazione', loadChildren: () => import('./features/registrazione/registrazione.module').then(m => m.RegistrazioneModule) },
-  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'customize', loadChildren: () => import('./features/customize/customize.module').then(m => m.CustomizeModule) }
 ];
 
 @NgModule({
