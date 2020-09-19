84fa614111ab7fc85d908f5a8821964f396a62e9
unknown <marcobert37@gmail.com>
2020-09-19 15:49:03 +0200

    form indirizzo

--- progetto/src/app/shared/components/step2-form/step2-form.component.ts
+++ progetto/src/app/shared/components/step2-form/step2-form.component.ts
@@ -1,4 +1,8 @@
+import { EventEmitter } from '@angular/core';
+import { Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Prodotto } from 'src/app/core/model/prodotto.interface';
 
 @Component({
   selector: 'app-step2-form',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Step2FormComponent implements OnInit {
 
-  constructor() { }
+  @Output()
+  formSubmitEvent: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  undoEvent: EventEmitter<any> = new EventEmitter();
+
+  indirizzoForm: FormGroup;
 
   ngOnInit(): void {
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.indirizzoForm = this.fb.group({
+        nome: ['',Validators.required],
+        cognome: ['',Validators.required],
+        cellulare: ['',Validators.required],
+        città: ['',Validators.required],
+        cap: ['',Validators.required],
+        indirizzo: ['',Validators.required],
+        numero: ['',Validators.required],
+        informazioni: ['',Validators.required],
+    });
+  }
+
+  confirmChanges() {
+    this.formSubmitEvent.emit(this.indirizzoForm.value);
+  }
+
+  cancel() {
+    this.undoEvent.emit(this.indirizzoForm.value);
   }
 
 }
